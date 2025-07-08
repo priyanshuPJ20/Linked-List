03d8ffe177605311c826a5d11a562a6b3afe54ef
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2025-07-09 00:08:17 +0530

    trial

---
+++ src/wer1.java
@@ -0,0 +1,16 @@
+public class wer1 {
+
+    public static void main(String[] args) {
+        wer list = new wer();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(5);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertLast(99);
+        list.insert(100,3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+    }
+}
