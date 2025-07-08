9d3ee93e815b6f17f18e8a9dfe441123253b204f
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2025-07-08 23:58:36 +0530

    first LinkedList

---
+++ src/LL1.java
@@ -0,0 +1,15 @@
+public class LL1 {
+    public static void main(String[] args) {
+      LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(5);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertLast(99);
+        list.insert(100,3);
+        list.display();
+      System.out.println(list.deleteFirst());
+      list.display();
+    }
+}
