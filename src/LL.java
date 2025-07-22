658f183cd2605eb63ee2c0a763d2dd15f2ba35d1
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2025-07-22 11:55:15 +0530

    deleting list

--- src/LL.java
+++ src/LL.java
@@ -42,7 +42,45 @@ public class LL {
         temp.next=node;
         size++;
     }
-
+    public int deleteLast(){
+        if(size<=1){
+            return deleteFirst();
+        }
+        Node secondLast=get(size-2);
+        int val= tail.value;
+        tail=secondLast;
+        tail.next=null;
+        return val;
+    }
+    public int delete(int index){
+        if(index==0){
+            return deleteFirst();
+        }
+        if(index==size-1){
+            return deleteLast();
+        }
+        Node prev=get (index-1);
+        int val=prev.next.value;
+        prev.next=prev.next.next;
+        return val;
+    }
+    public Node find(int value){
+        Node node =head;
+        while(node!=null) {
+        if(node.value==value){
+            return node;
+        }
+        node=node.next;
+        }
+        return null;
+    }
+    public Node get(int index){
+        Node node =head;
+        for (int i = 0; i <index ; i++) {
+            node =node.next;
+        }
+        return node;
+    }
     public int deleteFirst(){
         int val= head.value;
         head=head.next;
--- src/LL1.java
+++ src/LL1.java
@@ -3,13 +3,16 @@ public class LL1 {
       LL list = new LL();
         list.insertFirst(3);
         list.insertFirst(2);
-        list.insertFirst(5);
         list.insertFirst(8);
-        list.insertFirst(9);
+        list.insertFirst(17);
         list.insertLast(99);
         list.insert(100,3);
         list.display();
       System.out.println(list.deleteFirst());
+      list.display();
+      System.out.println(list.deleteLast());
+      list.display();
+      System.out.println(list.delete(2));
       list.display();
     }
 }
