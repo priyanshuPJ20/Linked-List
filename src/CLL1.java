3edf2eac3ba7258952c31a0d75ce928a685997f1
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2025-07-23 10:36:49 +0530

    DLL

---
+++ src/CLL1.java
@@ -0,0 +1,12 @@
+public class CLL1 {
+    public static void main(String[] args) {
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+        list.display();
+        list.delete(19);
+        list.display();
+    }
+}
--- src/DLL1.java
+++ src/DLL1.java
@@ -4,7 +4,7 @@ public class DLL1 {
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
-        list.insertFirst(17);
+        list.insertFirst(1);
         list.insertLast(99);
         list.insert(8,99);
         list.display();
