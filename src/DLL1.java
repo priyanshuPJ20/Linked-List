cd9a266d924f7857086695b8a3ec18130c240f92
priyanshuPJ20 <priyanshu19.kpl@gmail.com>
2025-07-23 10:34:14 +0530

    DLL

---
+++ src/DLL1.java
@@ -0,0 +1,12 @@
+public class DLL1 {
+    public static void main(String[] args) {
+        DLL list =new DLL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(8,99);
+        list.display();
+    }
+}
